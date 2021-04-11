c28d9f48ab6b74fa01f08d38830eb5547b80ca1c
ychoon94 <mrsiao2256@gmail.com>
2021-04-11 15:54:56 +0800

    completed L10

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true
+            cart: [],
+            premium: true,
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
+        },
+        removeFromCart(id){
+            const index = this.cart.indexOf(id)
+            if (index > -1) {
+                this.cart.splice(index, 1)
+            }
+        }
+    }
 })

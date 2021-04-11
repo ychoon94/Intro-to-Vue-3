1788161bcb9e996c78317bd93af9a7ec04fe4242
ychoon94 <mrsiao2256@gmail.com>
2021-04-11 12:50:44 +0800

    completed l8

--- main.js
+++ main.js
@@ -4,21 +4,36 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
+        }
+    },
+    computed: {
+        title() {
+            if (this.onSale){
+                return this.brand + ' ' + this.product + ' is on sale'
+            } else {
+                return this.brand + ' ' + this.product
+            }
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         }
     }
 })
